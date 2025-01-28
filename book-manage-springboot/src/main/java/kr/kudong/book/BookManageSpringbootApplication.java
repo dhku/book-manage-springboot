e4aab2b9e3590e72d8c888726ef7eb2fb5042470
구동혁 <dhku@naver.com>
2025-01-28 19:24:20 +0900

    first commit

---
+++ book-manage-springboot/src/main/java/kr/kudong/book/BookManageSpringbootApplication.java
@@ -0,0 +1,12 @@
+package kr.kudong.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManageSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManageSpringbootApplication.class, args);
+	}
+}
